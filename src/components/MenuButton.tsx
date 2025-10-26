fe41ba0b65bcd490c610e1bd3adaa689f59d6fa2
Sage295 <se731266@ucf.edu>
2025-10-25 23:47:44 -0400

    Create MenuButton.tsx

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
+import "./MenuButton.css";
+
+const MenuButton: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      {/* Menu icon button */}
+      <button
+        className="menu-button"
+        onClick={() => setOpen(!open)}
+        aria-label="Toggle menu"
+      >
+        <FontAwesomeIcon
+          icon={open ? faXmark : faBarsStaggered}
+          className="menu-icon"
+        />
+      </button>
+
+      {/* Sliding Holographic Menu */}
+      <div className={`holo-menu ${open ? "open" : ""}`}>
+        <ul>
+          
+          <li><a href="/skillbuilder"> Skill Builder</a></li>
+          <li><a href="/skillcheck"> Skill Check</a></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default MenuButton;
